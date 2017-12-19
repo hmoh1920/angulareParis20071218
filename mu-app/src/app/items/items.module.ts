24dcc8ec872c73767dcb2808e45a8be568746a2b
hmoh1920 <hmoh1920@hotmail.fr>
2017-12-19 13:53:14 +0100

    compenent item and compenent item list

--- mu-app/src/app/items/items.module.ts
+++ mu-app/src/app/items/items.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ItemComponent } from './components/item/item.component';
+import { ListItemsComponent } from './components/list-items/list-items.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ItemComponent, ListItemsComponent],
+  exports: [ItemComponent, ListItemsComponent]
 })
 export class ItemsModule { }
