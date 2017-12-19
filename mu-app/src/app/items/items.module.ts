7b71fdbfc0318bb95d53e6b25bf92edb59665dc9
hmoh1920 <hmoh1920@hotmail.fr>
2017-12-19 15:12:32 +0100

    add directive state

--- mu-app/src/app/items/items.module.ts
+++ mu-app/src/app/items/items.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 import { ItemComponent } from './components/item/item.component';
 import { ListItemsComponent } from './components/list-items/list-items.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   declarations: [ItemComponent, ListItemsComponent],
   exports: [ItemComponent, ListItemsComponent]
--- mu-app/src/app/shared/shared.module.ts
+++ mu-app/src/app/shared/shared.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './components/menu/menu.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StateDirective } from './directives/state/state.directive';
 
 @NgModule({
   imports: [
     CommonModule,
     NgbModule
   ],
-  declarations: [MenuComponent],
-  exports: [MenuComponent]
+  declarations: [MenuComponent, StateDirective],
+  exports: [MenuComponent, StateDirective]
 })
 export class SharedModule { }
